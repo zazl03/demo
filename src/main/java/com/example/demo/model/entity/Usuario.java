bf4c7345d68ab3c8c86a2a1a4c454369fd0c3fdc
zazl03 <rm99080@estudante.fieb.edu.br>
2026-06-02 12:29:47 -0300

    Camada model entity em construção

---
+++ src/main/java/com/example/demo/model/entity/Usuario.java
@@ -0,0 +1,22 @@
+package com.example.demo.model.entity;
+
+import com.example.demo.model.enums.TipoUsuario;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+    private String sexo;
+    private String logradouro;
+    private String cep;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private Boolean codStatus;
+    private TipoUsuario tipoUsuario;
+            ;
+
+}
---
+++ src/main/java/com/example/demo/model/enums/TipoUsuario.java
@@ -0,0 +1,11 @@
+package com.example.demo.model.enums;
+
+import java.sql.ClientInfoStatus;
+
+public class TipoUsuario {
+
+    ADMIN,
+    ClientInfoStatus,
+    FUNCIONARIO,
+
+}
